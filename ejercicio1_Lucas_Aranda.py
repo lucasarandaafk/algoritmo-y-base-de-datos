495ab9eb9e991e61a1724851b1c957efb1748508
lucas aranda <lucas.arandaet32@gmail.com>
2025-06-27 08:54:34 -0300

    Delete ejercicio1_Lucas_Aranda.py

--- ejercicio1_Lucas_Aranda.py
+++
@@ -1,9 +0,0 @@
-def busquedaL(a, valor):
-    for i in range(len(a)):
-        if a[i] == valor:
-            return i
-    return "no encontrado"
-
-numeros = [15, 33, 66, 99]
-print("resultado de búsqueda lineal (buscando 15):", busquedaL(numeros, 15)) 
-print("resultado de búsqueda lineal (buscando 99):", busquedaL(numeros, 99)) 
