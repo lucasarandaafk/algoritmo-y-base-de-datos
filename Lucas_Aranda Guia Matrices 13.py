a59823780a1fe82e213c9cab4d1027a8a906b2e7
lucas aranda <lucas.arandaet32@gmail.com>
2025-07-11 11:48:20 -0300

    Add files via upload

---
+++ Lucas_Aranda Guia Matrices 13.py
@@ -0,0 +1,27 @@
+def rotar_derecha(matriz):
+    filas = len(matriz)
+    columnas = len(matriz[0])
+
+    ultimo = matriz[filas - 1][columnas - 1]
+
+    for i in range(filas - 1, -1, -1):
+        for j in range(columnas - 1, -1, -1):
+            if j == 0:
+                if i != 0:
+                    matriz[i][j] = matriz[i - 1][columnas - 1]
+                else:
+                    matriz[i][j] = ultimo
+            else:
+                matriz[i][j] = matriz[i][j - 1]
+
+
+matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+rotar_derecha(matriz)
+
+for fila in matriz:
+    print(fila)
